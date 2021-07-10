2307bb2b1448762931723d4cff6693b1638be6cd
Sahar E.Hassan <sahar.e.hassan2015@gmail.com>
2021-07-11 01:46:28 +0200

    4-create footer, handle msgs container scroll, change color theme (#4)
    Co-authored-by: Sahar Elsayed <sahar.elsayed@trianglz.com>

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,14 @@
-const Button = () => {
-  return ( <p>Button</p> );
-}
- 
+import React from 'react';
+import "./styles.scss";
+
+/*
+  Button component for general used
+  @props: { theme, text, isDisabled}
+*/
+const Button = ({theme, text, isDisabled}) =>  ( 
+  <button disabled={isDisabled} className={`btn ${theme}`}>
+    {text}
+  </button>
+);
+
 export default Button;
